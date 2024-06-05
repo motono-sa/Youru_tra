61abb7d90dc23189893d1905c17866b96f85f579
Satoru Motono <samotono@gmail.com>
2024-06-05 16:50:43 +0900

    fix:動画系の画面追加、youtubeの検索条件変更

--- app/services/youtube_service.rb
+++ app/services/youtube_service.rb
@@ -4,18 +4,33 @@ class YoutubeService
 
   def initialize
     @api_key = ENV['YOUTUBE_API_KEY']
+    @keywords = ["exercise", "fitness", "training", "bodybuilding"]
+    @ng_keywords = ["バストアップ", "女性"]
   end
 
-  def search_videos(query)
+  def search_random_video(training_part_name)
+    query = generate_random_query(training_part_name)
     options = {
       query: {
         part: 'snippet',
         q: query,
         type: 'video',
         maxResults: 1,
+        regionCode: 'JP',
+        relevanceLanguage: 'ja',
+        videoDuration: 'medium', 
         key: @api_key
       }
     }
-    self.class.get('/search', options)
+    response = self.class.get('/search', options)
+    response['items'].first 
   end
+
+  private
+
+  def generate_random_query(training_part_name)
+    "#{training_part_name} #{keywords.sample} workout 自重 -#{ng_keywords.join(" -")}"
+  end
+
+  attr_reader :keywords, :ng_keywords
 end
